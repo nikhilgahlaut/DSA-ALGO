b942796bf216330c973d014b3ab934e04a97da2d
Nikhil Gahlaut <40818207+nikhilgahlaut@users.noreply.github.com>
2023-08-31 06:01:11 +0000

    addition of comments for proper readibilty

--- DSA/insertionSort.cpp
+++ DSA/insertionSort.cpp
@@ -4,9 +4,17 @@ using namespace std;
 
 //insertion sort function
 void insertionSort(int arr[], int n){
+
+//loop to traverse every element from 2nd element as 1st element is assumed as sorted array
     for(int i = 1 ; i< n ; i++){
+    
+//intializing each ele with current variable in every step
         int current = arr[i];
+
+//j is a variable pointing to last sorted element .
         int j = i - 1;
+
+//while loop is used to compare the element with all sorted element an inserting it at its place
         while(arr[j] > current && j >= 0){
             arr[j+1] = arr[j];
             j--;
