163e9117e1aa55f8bec79c5df03046184955609d
Nikhil Gahlaut <40818207+nikhilgahlaut@users.noreply.github.com>
2023-09-06 11:45:00 +0000

    QUICK SORT

---
+++ DSA/quickSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+using namespace std;
+int partition(int arr[], int first, int last){    // 12 18 16 20 30 35
+    int pivot = arr[last];
+    int  i = first - 1;
+    for(int j = first ; j < last ; j++){
+        if(arr[j] < pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[last]);
+    return i+1;
+}
+
+void quickSort(int arr[], int first, int last){
+    if(first>=last){
+        return;
+    }
+    int pi = partition(arr,first ,last);
+    quickSort(arr,first,pi-1);
+    quickSort(arr,pi + 1, last);
+}
+
+int main(){
+    int arr[] = {18,12,30,16,35,20};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    quickSort(arr, 0, n-1);
+    for(int i = 0 ; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
