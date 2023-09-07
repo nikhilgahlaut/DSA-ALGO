cb5ec0eb4f7d7818c5ea11f327678674c8aa1498
Nikhil Gahlaut <40818207+nikhilgahlaut@users.noreply.github.com>
2023-09-07 16:45:49 +0000

    Quick sort

--- DSA/quickSort.cpp
+++ DSA/quickSort.cpp
@@ -1,7 +1,10 @@
+//quick sort algorithm
 #include <iostream>
 
 
 using namespace std;
+
+//function for partitioning sub-array
 int partition(int arr[], int first, int last){    // 12 18 16 20 30 35
     int pivot = arr[last];
     int  i = first - 1;
@@ -15,6 +18,7 @@ int partition(int arr[], int first, int last){    // 12 18 16 20 30 35
     return i+1;
 }
 
+//quick sort function
 void quickSort(int arr[], int first, int last){
     if(first>=last){
         return;
@@ -27,7 +31,11 @@ void quickSort(int arr[], int first, int last){
 int main(){
     int arr[] = {18,12,30,16,35,20};
     int n = sizeof(arr)/sizeof(arr[0]);
+
+    //function call
     quickSort(arr, 0, n-1);
+
+    //printing sorted array
     for(int i = 0 ; i < n; i++){
         cout<<arr[i]<<" ";
     }
