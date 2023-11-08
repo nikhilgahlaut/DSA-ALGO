b86e1b174ace66e9cc7fd737657cc6033d5f7b33
Nikhil Gahlaut <40818207+nikhilgahlaut@users.noreply.github.com>
2023-11-08 16:11:45 +0000

    linkedList

--- DSA/linkedList.cpp
+++ DSA/linkedList.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<iostream> 
 using namespace std;
 
 class Node{
@@ -29,6 +29,49 @@ void insertAtTail(Node* &head, int val){
     temp->next = new_node;
 }
 
+//inserting at the given position
+void insertAtPosition(Node* &head, int pos,int val){
+    if(pos == 0){
+        insertAtHead(head,val);
+        return;
+    }
+    //if position is other than 0
+    Node* new_node = new Node(val);
+    Node* temp = head;
+    int current = 0;
+    while(current!=pos-1){
+        temp = temp->next;
+        current++;
+    }
+    //when current = pos-1
+    new_node ->next = temp->next;
+    temp->next = new_node; 
+}
+
+//deleting at head
+void deleteAtHead(Node* &head){
+    Node* temp = head;
+    head = head->next;
+    free(temp);
+}
+
+//deleting at position
+void deleteAtPosition(Node* &head,int pos){
+    if(pos==0){
+        deleteAtHead(head);
+        return;
+    }
+    Node* prev = head;
+    int curr = 0;
+    while(curr != pos-1){
+        prev = prev->next;
+        curr++;
+    }
+    Node* temp = prev->next;
+    prev->next = prev->next->next;
+    free(temp);
+}
+
 //display linkedlist
 void display(Node* head){
     Node *temp = head;
@@ -51,9 +94,18 @@ int main(){
     insertAtHead(head,4);
     display(head);
     
-    //at tail
+    //insert at tail
     insertAtTail(head,1);
     display(head);
+    insertAtPosition(head,1,5);
+    display(head);
+
+    //delete
+    deleteAtHead(head);
+    display(head);
+    deleteAtPosition(head,1);
+    display(head);
+
 
     return 0;
 }
